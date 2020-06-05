8e1bde80ae2ee05f0a22032ecf48aa0cb15743bf
Nathan Hillyer <nathanielhillyer@gmail.com>
2020-06-05 12:57:24 -0500

    Switch package name to rad-i. #7

--- module/setup.py
+++ module/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rad-i-nathanhillyer",
+    name="rad-i",
     version="0.0.1",
     author="Toth Technology",
     author_email="toth-tech@hillyer.me",
